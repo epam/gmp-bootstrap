213d46b6c8317688dea92d5f5171880f476ecfb6
Andrei Samuta <a.samuta@gmail.com>
2021-11-21 02:57:53 +0300

    Migrate to gradle 8

--- run_jar.bat
+++ run_jar.bat
@@ -1,5 +1,4 @@
-call gradle clean build -Pweb=false
-#Path to your base folder
-set WORK_FOLDER=
+REM Path to your base folder. We use current in this example.
+set WORK_FOLDER=%CD%
 java -Dgmp.home=file:///%WORK_FOLDER%/build/gmp-home/ -Dgmp.log.home=%WORK_FOLDER%/build/gmp-home/log -jar ./build/gmp-home/gmp-bootstrap-0.2-SNAPSHOT.jar  dev@sample-sub-module/03-use-freemarker.groovy dev@sample-sub-module/01-accessing-config.groovy
 
