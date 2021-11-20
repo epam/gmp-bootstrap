cb8e4c131fea23efc51edd70786e6f99bb9db98e
Andrei Samuta <a.samuta@gmail.com>
2021-11-21 01:46:33 +0300

    Update GMP dependencies.

--- run_jar.bat
+++ run_jar.bat
@@ -1,3 +1,5 @@
 call gradle clean build -Pweb=false
-java -Dgmp.home=./build/gmp-home  -jar ./build/gmp-home/gmp-bootstrap-0.1-SNAPSHOT.jar  dev@sample-sub-module/03-use-freemarker.groovy dev@sample-sub-module/01-accessing-config.groovy
+#Path to your base folder
+set WORK_FOLDER=
+java -Dgmp.home=file:///%WORK_FOLDER%/build/gmp-home/ -Dgmp.log.home=%WORK_FOLDER%/build/gmp-home/log -jar ./build/gmp-home/gmp-bootstrap-0.2-SNAPSHOT.jar  dev@sample-sub-module/03-use-freemarker.groovy dev@sample-sub-module/01-accessing-config.groovy
 
